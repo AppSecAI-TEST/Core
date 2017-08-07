cdabb5cd8d470e8eeb343231c5463320ba7f6261
Maksim <maksim.lyahovets@service-it.by>
2017-08-07 14:41:03 +0300

    Base

--- src/main/model/BaseEntity.java
+++ src/main/model/BaseEntity.java
@@ -1,55 +1,23 @@
 package main.model;
 
-import java.util.Set;
-
-public abstract class BaseObject {
+public abstract class BaseEntity {
     Integer id;
-    String name;
-    private Integer salary;
 
-    private Set<Skill> skills;
-
-
-    public BaseObject(Integer id, String name, Integer salary, Set<Skill> skills) {
+    public BaseEntity(Integer id) {
         this.id = id;
-        this.name = name;
-        this.salary = salary;
-        this.skills = skills;
     }
 
-    public BaseObject() {
+    public BaseEntity() {
 
     }
 
     public Integer getId() {
+
         return id;
     }
 
     public void setId(Integer id) {
+
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Skill> getSkills() {
-        return skills;
-    }
-
-    public void setSkills(Set<Skill> skills) {
-        this.skills = skills;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
     }
 }
