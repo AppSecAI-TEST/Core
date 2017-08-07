18e8e6db4b4b3ccdf49d674e7a27506a42d72874
Maksim <maksim.lyahovets@service-it.by>
2017-08-07 12:09:55 +0300

    Cutomer

--- src/main/model/Customer.java
+++ src/main/model/Customer.java
@@ -1,40 +1,40 @@
-//package main.model;
-//
-//import java.util.Set;
-//
-//public class Customer extends BaseObject {
-//
-//    private Set<Project> customerProjects;
-//
-//    public Customer() {
-//
-//    }
-//
-//    public Customer(int id, String name) {
-//        super(id, name);
-//    }
-//
-//    public Set<Project> getCustomerProjects() {
-//        return customerProjects;
-//    }
-//
-//    public void setCustomerProjects(Set<Project> customerProjects) {
-//        this.customerProjects = customerProjects;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringBuilder builder = new StringBuilder("Customer{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", projects:\n");
-//        for (Project project : customerProjects) {
-//            builder.append("Project{id=");
-//            builder.append(project.getId());
-//            builder.append(", name='");
-//            builder.append(project.getName());
-//            builder.append("\'},\n");
-//        }
-//        return builder.substring(0, builder.length() - 2) + "}\n";
-//    }
-//}
+package main.model;
+
+import java.util.Set;
+
+public class Customer extends BaseObject {
+
+    private Set<Project> customerProjects;
+
+    public Customer() {
+
+    }
+
+    public Customer(Integer id, String name, Set<Skill> skills, Integer experience, Integer salary) {
+        super(id, name, salary, skills);
+    }
+
+    public Set<Project> getCustomerProjects() {
+        return customerProjects;
+    }
+
+    public void setCustomerProjects(Set<Project> customerProjects) {
+        this.customerProjects = customerProjects;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", projects:\n");
+        for (Project project : customerProjects) {
+            builder.append("Project{id=");
+            builder.append(project.getId());
+            builder.append(", name='");
+            builder.append(project.getName());
+            builder.append("\'},\n");
+        }
+        return builder.substring(0, builder.length() - 2) + "}\n";
+    }
+}
